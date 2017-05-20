84e283c21a604604d24518ba5ea9361f61e403fb
Tscheiki <markus.tscheik@gmail.com>
2017-05-20 14:50:47 +0200

    removed startMigration bash script

--- migration/startMigration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ROOT_PATH=$1
-ID=$2
-
-echo "Starting migration"
-php $ROOT_PATH'migration/createMigration.php' $ID
-
-#ansible-playbook -i ansible/hosts -s ansible/migrationPlaybook.yml
